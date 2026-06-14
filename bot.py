9b7c8640fd64f1ed2b04041f591dd1815bc19c6b
tarikumulat <tarikuuumulattt@gmail.com>
2026-06-14 16:04:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,65 @@
+
 import os
 import telebot
+from telebot import types
 
 TOKEN = os.getenv("BOT_TOKEN")
 
 bot = telebot.TeleBot(TOKEN)
 
 
-# /start command
+# START
 @bot.message_handler(commands=['start'])
 def start(message):
 
+    # Show logo first
     bot.send_photo(
         message.chat.id,
-        photo=open("logo.png", "rb"),
-        caption="🚀 Welcome to WagaBid!"
+        photo=open("logo.png", "rb")
     )
+
+    # Language selector buttons
+    markup = types.ReplyKeyboardMarkup(
+        resize_keyboard=True
+    )
+
+    markup.row("🇬🇧 English")
+    markup.row("🇪🇹 አማርኛ")
+    markup.row("🟢 Afaan Oromo")
+
+    bot.send_message(
+        message.chat.id,
+        "Choose Language\n\n"
+        "ቋንቋ ይምረጡ\n\n"
+        "Afaan filadhaa",
+        reply_markup=markup
+    )
+
+
+# LANGUAGE HANDLER
+@bot.message_handler(func=lambda message: True)
+def language_selector(message):
+
+    if message.text == "🇬🇧 English":
+
+        bot.send_message(
+            message.chat.id,
+            "Welcome to WagaBid 🚀"
+        )
+
+    elif message.text == "🇪🇹 አማርኛ":
+
+        bot.send_message(
+            message.chat.id,
+            "ወደ WagaBid እንኳን በደህና መጡ 🚀"
+        )
+
+    elif message.text == "🟢 Afaan Oromo":
+
+        bot.send_message(
+            message.chat.id,
+            "Baga gara WagaBid dhuftan 🚀"
+        )
 
 
 print("🤖 Bot is running...")
